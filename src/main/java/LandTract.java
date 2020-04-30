5f50d0a0299231b3fb78e70a9a958c8320f6f04c
mason <mason@dhcp-172-16-237-184.dyn.pittstate.edu>
2020-04-30 14:26:17 -0500

    Fixed naming issue

---
+++ src/main/java/LandTract.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mason
+ */
+public class LandTract {
+    
+    private double tractLength;
+    private double tractWidth;
+    private double area;
+    public LandTract() {
+        area = 0.0;
+    }
+    public LandTract(double length, double width) {
+        tractLength = length;
+        tractWidth = width;
+    }
+    public double getLength(double length) {
+        return length;
+    }
+    public double getWidth(double width) {
+        return width;
+    }
+    public double getArea(double length, double width) {
+        area = length * width;
+        return area;
+    }
+    public double getArea() {
+        area = this.tractLength * this.tractWidth;
+        return area;
+    }
+    public boolean equals(LandTract tract2) {
+        return this.area == tract2.area;
+    }
+    public String toString(int area) {
+        return "Area: " + area;
+    }
+}
+
